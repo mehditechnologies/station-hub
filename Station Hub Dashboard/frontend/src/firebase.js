7c2bb9d05a0f4f449a8f97965afb5fe8b2c21c2b
Ahmad Abdullah <abdullahhrajput4943@gmail.com>
2026-06-09 10:57:44 +0500

    Authentication With Firebase Part 1

---
+++ Station Hub Dashboard/frontend/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrHlHg7jzPAdf2cHC5MZv3o1O5OgLnBq4",
+  authDomain: "stationhubproject.firebaseapp.com",
+  projectId: "stationhubproject",
+  storageBucket: "stationhubproject.firebasestorage.app",
+  messagingSenderId: "522728415702",
+  appId: "1:522728415702:web:633c3cfbc6a7ff6b3d0477",
+  measurementId: "G-F08JKT8TKP"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
